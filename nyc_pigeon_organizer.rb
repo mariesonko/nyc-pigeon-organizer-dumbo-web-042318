22afaaa10fe9c738d16b613e06c6b8d3a970cafd
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 21:39:38 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  data = {}
+  pigeon_hash = {}
+  data.each do |place, pigeon|
+    pigeon.each do |name, pigeon_arr|
+      pigeon_arr.each do |pigeon_name|
+        if !pigeon_hash.has_key? (pigeon_name)
+          pigeon_hash[pigeon_name] = {}
+        elsif
+          !pigeon_hash[pigeon_name].has_key? (place)
+            pigeon_hash[pigeon_name][place] = []
+          else
+            !pigeon_hash[pigeon_name].include? (name)
+              pigeon_hash[pigeon_name][place] << name.to_s
+        end
+      end
+    end
+    end
+    pigeon_hash
 end
