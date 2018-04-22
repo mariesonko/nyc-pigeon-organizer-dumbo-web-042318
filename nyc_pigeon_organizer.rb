4c5447c00a13a61ad4d7372d7bf4a5f3fd008c14
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 21:41:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |place, pigeon|
+    pigeon.each do |name, pigeon_arr|
+      pigeon_arr.each do |pigeon_name|
+
+        if !pigeon_hash.has_key? (pigeon_name)
+          pigeon_hash[pigeon_name] = {}
+        end
+        
+        if !pigeon_hash[pigeon_name].has_key? (place)
+            pigeon_hash[pigeon_name][place] = []
+          end
+
+            if !pigeon_hash[pigeon_name].include? (name)
+              pigeon_hash[pigeon_name][place] << name.to_s
+            end
+        end
+      end
+    end
+    pigeon_hash
 end
